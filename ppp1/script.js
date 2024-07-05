4729e303e375a014c3402142d8afc2cd043fd63e
Abel Alvarado <g1911032@gmail.com>
2024-07-05 11:57:04 -0300

    prueba

---
+++ ppp1/script.js
@@ -0,0 +1,14 @@
+//para el desplazamiento suave al hacer clic en los enlaces del menú
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
